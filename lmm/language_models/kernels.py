cf1b081f18f9e0bce0433c79d352105f01c60dab
Roberto Viviani home new <roberto.viviani@uibk.ac.at>
2025-09-20 07:32:55 +0200

    kernels, revision docstring and explanations

--- lmm/language_models/kernels.py
+++ lmm/language_models/kernels.py
@@ -1,5 +1,5 @@
 """
-Definition of language models chat kernels prompts.
+Definition of language models chat kernels prompt templates.
 
 This module contains a set of predefined prompt templates,
 
@@ -9,8 +9,8 @@ This module contains a set of predefined prompt templates,
     - "query_with_context"
     - "check_content"
 
-These prompts may be retrieved from a module-level dictionary, as
-shown in the example below.
+These prompts may be retrieved from the module-level dictionary
+`kernel_prompts`, as shown in the example below.
 
 **Example**:
 
@@ -19,7 +19,17 @@ shown in the example below.
     prompt_template: str = kernel_prompts["summarizer"]
     ```
 
-New prompt templates may be added dynamically to the dictionary.
+Note that the prompot template text may be to be adapted to the
+framework to be used. For example, for Lanchain,
+
+    ```python
+    from langchain_core.prompts import PromptTemplate
+    prompt: PromptTemplate = PromptTemplate.from_template(
+        kernel_prompts["summarizer"]
+    )
+    ```
+
+New prompt text templates may be added dynamically to the dictionary.
 
 **Example**:
 
@@ -41,7 +51,8 @@ in the library, like the `create_kernel` functions.
 from typing import Literal
 from .lazy_dict import LazyLoadingDict
 
-# Define here the kernel supported by the package.
+# Define here the kernels supported by the package,
+# all of them chat kernels.
 KernelNames = Literal[
     "summarizer",
     "question_generator",
@@ -51,7 +62,8 @@ KernelNames = Literal[
 ]
 
 
-# A functional returning the prompts for these kernels.
+# A functional returning the prompts for these kernels. This is used for
+# the typed dictionary containing the prompt texts.
 def _get_prompts(kernel_name: KernelNames) -> str:
     match kernel_name:
         case "summarizer":  # --- kernel case definition
@@ -115,7 +127,7 @@ TEXT CONTENT:
             raise ValueError(f"Invalid kernel: {kernel_name}")
 
 
-# a global object store to get the prompts
+# a module-level typed dictionary for the preformed prompts
 kernel_prompts = LazyLoadingDict(_get_prompts)
 
 
@@ -123,4 +135,8 @@ def create_prompt(prompt_template: str, prompt_name: str) -> None:
     """
     Adds a custom prompt template to the prompt dictionary.
     """
+
+    # We abuse the lack of run-time checks for Literals here. We do
+    # this because we want the availability of the preformed prompts
+    # given by Literal but also the flexibility to add new prompts.
     kernel_prompts[prompt_name] = prompt_template  # type: ignore
