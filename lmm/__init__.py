7bcb6f6a933ad645a523a04fb321cc269b4c0565
Roberto Viviani home new <roberto.viviani@uibk.ac.at>
2025-11-05 20:10:29 +0100

    lm/__init__, fix import bug

--- lmm/__init__.py
+++ lmm/__init__.py
@@ -1,7 +1,7 @@
 def _default_config() -> None:
     # Create a default config.toml file, if there is none.
     from pathlib import Path
-    from config.config import (
+    from .config.config import (
         DEFAULT_CONFIG_FILE,
         create_default_config_file,
     )
